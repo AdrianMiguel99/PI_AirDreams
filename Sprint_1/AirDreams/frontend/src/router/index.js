df7b3e5ff45ed4ee6f9d6563ac3af024c8fe7364
Alexa Alpízar Mora <aalpizar@MacBook-Pro-de-Alexa-2.local>
2026-04-24 17:48:41 -0600

    Se agrega un router para el Log in.

---
+++ Sprint_1/AirDreams/frontend/src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LandingPage from '../views/LandingPage.vue'
+import Login from '../views/Login.vue'
+
+const routes = [
+  { path: '/', component: LandingPage },
+  { path: '/login', component: Login }
+]
+
+export default createRouter({
+  history: createWebHistory(),
+  routes
+})
