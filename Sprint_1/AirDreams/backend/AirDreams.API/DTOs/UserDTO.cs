27b84f8f667d2c8ea5343feb2f62a15c5385dd09
Dylan Torres <dylantorres538@gmail.com>
2026-04-25 20:16:31 -0600

    Fix dto attributes

--- Sprint_1/AirDreams/backend/AirDreams.API/DTOs/UserDTO.cs
+++ Sprint_1/AirDreams/backend/AirDreams.API/DTOs/UserDTO.cs
@@ -3,8 +3,8 @@ namespace AirDreams.API.DTOs
     public class UserDTO
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FullName { get; set; }
         public string Email { get; set; }
-        public string Role { get; set; }
+        public bool isOperator { get; set; }
     }
 }
