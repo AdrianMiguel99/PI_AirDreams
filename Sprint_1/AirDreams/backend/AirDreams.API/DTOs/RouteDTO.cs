9a208282db40a08c67ec0231805e43b07e00a325
Dylan Torres <dylantorres538@gmail.com>
2026-04-25 21:20:22 -0600

    Add Route and Flight DTO

---
+++ Sprint_1/AirDreams/backend/AirDreams.API/DTOs/RouteDTO.cs
@@ -0,0 +1,14 @@
+namespace AirDreams.API.DTOs
+{
+    public class RouteDTO
+    {
+        public string Id { get; set; }
+        public string DepartureTime { get; set; }
+        public string ArrivalTime { get; set; }
+        public string Duration { get; set; }
+        public AirportDTO DepartureAirport { get; set; }
+        public AirportDTO ArrivalAirport { get; set; }
+        public decimal TouristPrice { get; set; }
+        public decimal FirstClassPrice { get; set; }
+    }
+}
