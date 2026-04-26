7b28036857288922955f532083b0a57baf3ce604
Dylan Torres <dylantorres538@gmail.com>
2026-04-25 19:24:19 -0600

    Add Airport Controller and DTO

--- Sprint_1/AirDreams/backend/AirDreams.API/DTOs/AirportDTO.cs
+++ Sprint_1/AirDreams/backend/AirDreams.API/DTOs/AirportDTO.cs
@@ -1,7 +1,8 @@
-namespace AirDreams.API.DTOs
+namespace Airport.API.DTOs
 {
     public class AirportDTO
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Code { get; set; }
         public string City { get; set; }
