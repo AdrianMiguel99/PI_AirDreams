037e087acdae8f7d5b7d3c2b01dce8ca0ebd8bfa
Dylan Torres <dylantorres538@gmail.com>
2026-04-25 21:20:42 -0600

    Fix sintaxis error on DTOs

--- Sprint_1/AirDreams/backend/AirDreams.API/DTOs/AirportDTO.cs
+++ Sprint_1/AirDreams/backend/AirDreams.API/DTOs/AirportDTO.cs
@@ -2,7 +2,6 @@ namespace AirDreams.API.DTOs
 {
     public class AirportDTO
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Code { get; set; }
         public string City { get; set; }
