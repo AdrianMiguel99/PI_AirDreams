d27b98d3824374c4e92047e59a513e798a69ad88
Dylan Torres <dylantorres538@gmail.com>
2026-04-25 21:20:42 -0600

    Fix sintaxis error on DTOs

--- Sprint_1/AirDreams/backend/AirDreams.API/DTOs/AircraftDTO.cs
+++ Sprint_1/AirDreams/backend/AirDreams.API/DTOs/AircraftDTO.cs
@@ -2,7 +2,7 @@ namespace AirDreams.API.DTOs
 {
     public class AircraftDTO
     {
-        public int Id { get; set; }
+        public string PlateNumber { get; set; }
         public string Model { get; set; }
         public int SeatsAmount { get; set; }
         public int Weight { get; set; }
