642b350693e74f111b188930cc23720d62afe14a
Dylan Torres <dylantorres538@gmail.com>
2026-04-25 21:37:28 -0600

    Add endpoint getters by id for aircraft, airport and user

--- Sprint_1/AirDreams/backend/AirDreams.API/Controllers/UserController.cs
+++ Sprint_1/AirDreams/backend/AirDreams.API/Controllers/UserController.cs
@@ -22,11 +22,20 @@ namespace AirDreams.API.Controllers
         {
             // TODO : User service and repository to get all users
             var users = userService.GetAllUsers();
-            if (users == null || users.Count == 0)
-            {
-                return NotFound("No users found");
-            }
             return Ok(users);
+        }
+
+        // GET: api/User/{id}
+        [HttpGet("{id}")]
+        public ActionResult<UserDTO> GetById(int id)
+        {
+            // TODO : User service and repository to get a user by id
+            var user = userService.GetUserById(id);
+            if (user == null)
+            {
+                return NotFound(new { message = "User not found" });
+            }
+            return Ok(user);
         }
 
     }
