6f04cd77f421196dfacaa7804baf82e6842b995c
adrianmiguel99 <adrian99xd@gmail.com>
2026-04-26 17:48:27 -0600

    Se completan las opciones del formulario

--- Sprint_1/AirDreams/frontend/src/components/AddAirplaneForm.vue
+++ Sprint_1/AirDreams/frontend/src/components/AddAirplaneForm.vue
@@ -29,27 +29,56 @@
     </div>
 
     <!--Formulario de registro de aeronave-->
-    <div class="d-flex justify-content-center align-items-center vh-75 mt-4">
-        <div class="letra_semibold p-3 shadow" style="max-width: 400px; width: 40%;">
+    <div class="d-flex justify-content-center align-items-center mt-4">
+        <div class="letra_semibold p-3 shadow" style="max-width: 700px; width: 40%; display: flex; gap:120px">
+            <!--Lado izquierdo del formulario-->
             <form>
                 <div class="mb-3">
                     <!-- TODO: QUE NO SE REPITA EN EL SISTEMA-->
                     <label for="matricula" class="form-label">Matrícula (10 caracteres max)</label>
-                    <input type="text" maxlength="10" class="form-control ." id="matricula" placeholder="Ejm: TI-BFJ">
+                    <input type="text" maxlength="10" class="form-control ." id="matricula" placeholder="Ejm: TI-BFJ" style="color: #384467;">
                 </div>
                 <div class="mb-3">
                     <label for="peso_general" class="form-label">Peso (kg)</label>
-                    <input type="text" class="form-control" id="peso_general" placeholder="Ejm: 1000">
+                    <input type="number" class="form-control" id="peso_general" placeholder="Ejm: 1000" style="color: #384467;">
                 </div>
                 <div class="mb-3">
                     <label for="peso_equipaje" class="form-label">Peso Equipaje (kg)</label>
-                    <input type="number" class="form-control" id="peso_equipaje" placeholder="Ejm: 500">
+                    <input type="number" class="form-control" id="peso_equipaje" placeholder="Ejm: 500" style="color: #384467;">
                 </div>
                 <div class="mb-3">
-                    <label for="peso_equipaje" class="form-label">Peso Equipaje (kg)</label>
-                    <input type="number" class="form-control" id="peso_equipaje" placeholder="Ejm: 500">
+                    <label for="tipo_aeronave" class="form-label">Seleccione tamaño de Aeronave</label>
+                    <select id="Tipo de Aeronave" name="tipo_aeronave" class="form-select" style="color: #384467;">
+                        <option value="pequena">Pequeña</option>
+                        <option value="mediana">Mediana</option>
+                        <option value="grande">Grande</option>
+                    </select>
                 </div>
-                <button type="submit" class="btn colores_invertidos w-100" style="background-color: #384467; font-weight: semi-bold">Registrar Aeronave</button>
+                <div class="mb-3">
+                    <label for="asientos_por_fila" class="form-label">Cantidad de Asientos por Fila</label>
+                    <input type="number" class="form-control" id="asientos_por_fila" placeholder="Ejm: 5" style="color: #384467;">
+                </div>
+            </form>
+            <!--Lado derecho del formulario-->
+            <form>
+                <div class="mb-3">
+                    <!-- TODO: QUE NO SE REPITA EN EL SISTEMA-->
+                    <label for="fabricante" class="form-label">Fabricante de Aeronave</label>
+                    <input type="text" class="form-control ." id="fabricante" placeholder="Ejm: TI-BFJ" style="color: #384467;">
+                </div>
+                <div class="mb-3">
+                    <label for="asientos_economicos" class="form-label">Cantidad de Asientos Económicos</label>
+                    <input type="number" class="form-control" id="asientos_economicos" placeholder="Ejm: 100" style="color: #384467;">
+                </div>
+                <div class="mb-3">
+                    <label for="asientos_FirstClass" class="form-label">Cantidad de Asientos de Primera Clase</label>
+                    <input type="number" class="form-control" id="asientos_FirstClass" placeholder="Ejm: 50" style="color: #384467;">
+                </div>
+                <div class="mb-3">
+                    <label for="filas_aeronave" class="form-label">Cantidad de Filas en la Aeronave</label>
+                    <input type="number" class="form-control" id="filas_aeronave" placeholder="Ejm: 20" style="color: #384467;">
+                </div>
+                <button type="submit" class="btn colores_invertidos w-100" style="background-color: #384467; font-weight: semi-bold; margin-top: 30px;">Registrar Aeronave</button>
             </form>
         </div>
     </div>
@@ -64,6 +93,7 @@
 </script>
 
 <style scoped>
+
     .letra_bold {
         font-family:  'Inter', sans-serif;
         color: #384467;
