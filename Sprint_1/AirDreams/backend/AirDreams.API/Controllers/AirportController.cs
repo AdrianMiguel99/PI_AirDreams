08370c327a8c2ad8f6ccdb18da05a58a9405249b
Dylan Torres <dylantorres538@gmail.com>
2026-04-25 19:24:19 -0600

    Add Airport Controller and DTO

--- Sprint_1/AirDreams/backend/AirDreams.API/Controllers/AirportController.cs
+++ Sprint_1/AirDreams/backend/AirDreams.API/Controllers/AirportController.cs
@@ -9,33 +9,24 @@ namespace AirDreams.API.Controllers
     [ApiController]
     public class AirportController : ControllerBase
     {
-        private readonly AirportService airportService;
+        private readonly IAirportService airportService;
 
-        public AirportController(AirportService airportService)
+        public AirportController(IAirportService airportService)
         {
             this.airportService = airportService;
         }
 
-        // GET: api/Airport
+        // GET: api/Airport for list
         [HttpGet]
         public ActionResult<List<AirportDTO>> GetAll()
         {
             // TODO : Airport service and repository to get all airports
             var airports = airportService.GetAllAirports();
-            return Ok(airports);
-        }
-
-        // GET: api/Airport/{id}
-        [HttpGet("{id}")]
-        public ActionResult<AirportDTO> GetById(string id)
-        {
-            // TODO : Airport service and repository to get an airport by id
-            var airport = airportService.GetAirportById(id);
-            if (airport == null)
+            if (airports == null || airports.Count == 0)
             {
-                return NotFound(new { message = "Airport not found" });
+                return NotFound("No airports found");
             }
-            return Ok(airport);
+            return Ok(airports);
         }
 
     }
