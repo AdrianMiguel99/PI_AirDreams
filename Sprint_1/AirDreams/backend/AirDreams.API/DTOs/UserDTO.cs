da2fee3b88e9f1b1fb8213bc8222872831a50ea8
Dylan Torres <dylantorres538@gmail.com>
2026-04-27 23:44:16 -0600

    Add User service

--- Sprint_1/AirDreams/backend/AirDreams.API/DTOs/UserDTO.cs
+++ Sprint_1/AirDreams/backend/AirDreams.API/DTOs/UserDTO.cs
@@ -2,7 +2,6 @@ namespace AirDreams.API.DTOs
 {
     public class UserDTO
     {
-        public int Id { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
         public bool IsOperator { get; set; }
---
+++ Sprint_1/AirDreams/backend/AirDreams.API/Services/UserService.cs
@@ -0,0 +1,26 @@
+using AirDreams.API.DTOs;
+using AirDreams.API.Repositories;
+using AirDreams.API.Services;
+
+namespace AirDreams.API.Services
+{
+    public class UserService : IService<UserDTO>
+    {
+        private readonly IRepository<UserDTO> userRepository;
+
+        public UserService(IRepository<UserDTO> userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public List<UserDTO> GetAll()
+        {
+            return userRepository.GetAll();
+        }
+
+        public UserDTO GetById(string id)
+        {
+            return userRepository.GetById(id);
+        }
+    }
+}
