b8ed8e1e217622fd9edc25f29af784a3a983fc0a
Dylan Torres <dylantorres538@gmail.com>
2026-05-03 01:50:37 -0600

    Fix routing and Add styles to users list page

--- Sprint_1/AirDreams/frontend/src/components/LandingPageAdmin/LandingPageAdmin.vue
+++ Sprint_1/AirDreams/frontend/src/components/LandingPageAdmin/LandingPageAdmin.vue
@@ -59,7 +59,7 @@ const navigateTo = (path) => {
         <section>
             <h2>Administración usuarios</h2>
             <div class="cards">
-            <div class="card" @click="navigateTo('/registro')">
+            <div class="card" @click="navigateTo('/admin/Usuarios')">
                 <img :src="usersIcon" alt="Users" class="icon blue">
                 <div>
                 <h3>Usuarios</h3>
