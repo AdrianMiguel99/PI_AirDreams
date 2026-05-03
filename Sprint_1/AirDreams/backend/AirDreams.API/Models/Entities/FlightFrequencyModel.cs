a01b7233362f1742f241f50ff452adabcce77a2c
JosueOrtiz1223 <Josue.ortiz@ucr.ac.cr>
2026-05-02 18:01:10 -0600

    Se agrega IService.cs y los controllers que hacian falta

---
+++ Sprint_1/AirDreams/backend/AirDreams.API/Models/Entities/FlightFrequencyModel.cs
@@ -0,0 +1,12 @@
+namespace AirDreams.API.Models
+{
+    public class FlightFrequencyModel
+    {
+        public string DayOfWeek { get; set; } = string.Empty;
+        public TimeSpan DepartureTime { get; set; }
+        public TimeSpan EstimatedArrivalTime { get; set; }
+        public DateTime StartingDate { get; set; }
+        public DateTime EndingDate { get; set; }
+        public bool Active { get; set; }
+    }
+}
