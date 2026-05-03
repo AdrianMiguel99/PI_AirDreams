c3c128663a60e6676b622fe98cf904fe9a0325c5
Alexa Alpízar Mora <aalpizar@MacBook-Pro-de-Alexa-2.local>
2026-05-02 21:03:50 -0600

    Se crea un servicio de registro de  usuarios para poder registrar

---
+++ Sprint_1/AirDreams/backend/AirDreams.API/Services/UserService.cs
@@ -0,0 +1,32 @@
+namespace AirDreams.API.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _userRepository;
+
+        public UserService()
+        {
+            _userRepository = new UserRepository();
+        }
+
+        public RegisterUserModel Register(RegisterModel model)
+        {
+            var user = new RegisterUserModel
+            {
+                NombreCompleto = model.NombreCompleto,
+                TipoUsuario = model.TipoUsuario,
+                Correo = model.Correo,
+                Cedula = model.Cedula,
+                emailConfirmed = false
+            };
+
+            var CreatedUserId = _userRepository.Create(user);
+            return user;
+        }
+
+        public UserModel GetUserByEmail(string email)
+        {
+            return _users.FirstOrDefault(u => u.Correo == email);
+        }
+    }
+}
