ce90b60d272e4f865c26eb2bad3aa8bfbdf4b947
Alexa Alpízar Mora <aalpizar@MacBook-Pro-de-Alexa-2.local>
2026-05-05 07:55:49 -0600

    Se resuelven conflictos

--- Sprint_1/AirDreams/backend/AirDreams.API/Models/RegisterModel.cs
+++ Sprint_1/AirDreams/backend/AirDreams.API/Models/RegisterModel.cs
@@ -2,12 +2,12 @@ namespace AirDreams.API.Models
 {
     public class RegisterModel
     {
-        public string NombreCompleto { get; set; }
+        public string NombreCompleto { get; set; } = "";
 
-        public string TipoUsuario { get; set; }
+        public string TipoUsuario { get; set; } = "";
 
-        public string Correo { get; set; }
-
-        public string Cedula { get; set; }
+        public string Correo { get; set; } = "";
+        
+        public string Cedula { get; set; } = "";
     }
 }
