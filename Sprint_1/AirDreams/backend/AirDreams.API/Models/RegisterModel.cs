b8c60b092490e079b5128f959137fbd21d8de785
Alexa Alpízar Mora <aalpizar@MacBook-Pro-de-Alexa-2.local>
2026-05-05 17:53:32 -0600

    Estandarizar a inglés

--- Sprint_1/AirDreams/backend/AirDreams.API/Models/RegisterModel.cs
+++ Sprint_1/AirDreams/backend/AirDreams.API/Models/RegisterModel.cs
@@ -2,12 +2,12 @@ namespace AirDreams.API.Models
 {
     public class RegisterModel
     {
-        public string NombreCompleto { get; set; } = "";
+        public string FullName { get; set; } = "";
 
-        public string TipoUsuario { get; set; } = "";
+        public string Role { get; set; } = "";
 
-        public string Correo { get; set; } = "";
+        public string Email { get; set; } = "";
         
-        public string Cedula { get; set; } = "";
+        public string id { get; set; } = "";
     }
 }
