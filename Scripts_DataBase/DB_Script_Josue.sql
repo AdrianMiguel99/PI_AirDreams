92ac0c9e1990927dd66983f9742ef75d7cad5de4
JosueOrtiz1223 <Josue.ortiz@ucr.ac.cr>
2026-05-17 15:09:14 -0600

    Se movio el estado de la ruta a estado de vuelo.

--- Scripts_DataBase/DB_Script_Josue.sql
+++ Scripts_DataBase/DB_Script_Josue.sql
@@ -4,3 +4,14 @@ GO
 Alter Table FlightFrequency
 ADD Constraint DF_FlightFrequency_StartingDate
 DEFAULT CAST(GETDATE() AS DATE) FOR startingDate;
+
+Alter table Route
+DROP CONSTRAINT CK__Route__routeStat__49C3F6B7;
+
+Alter table Route
+DROP COLUMN routeState;
+
+Alter table Flight
+ADD flightState VARCHAR(30) NOT NULL
+CHECK (flightState IN ('On-time', 'Boarding', 'Delayed', 'Canceled', 'In-Flight', 'Landed'))
+Default 'On-time';
