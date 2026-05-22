bfc504b111a05451c8f4b276d5772ac835d53cfb
JoseDavidCS <jcarrillosibaja@gmail.com>
2026-05-22 00:51:19 -0600

    Agregada la funcion para la edicion de Aeropuertos

---
+++ Sprint_1/AirDreams/backend/AirDreams.API/Models/Dto/UpdateAirportDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AirDreams.API.Models.Dtos
+{
+    public class UpdateAirportDto
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [MaxLength(200, ErrorMessage = "El nombre no puede exceder los 200 caracteres.")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
