c10921d36da7ffa604b716416b5c53db76f857ce
JosueOrtiz1223 <Josue.ortiz@ucr.ac.cr>
2026-05-31 17:23:43 -0600

    Se cambio un poco el script para que sea mas legible

--- Scripts_DataBase/DB_Script_Josue.sql
+++ Scripts_DataBase/DB_Script_Josue.sql
@@ -16,60 +16,39 @@ ADD flightState VARCHAR(30) NOT NULL
 CHECK (flightState IN ('On-time', 'Boarding', 'Delayed', 'Canceled', 'In-Flight', 'Landed'))
 Default 'On-time';
 
-
-
-Select * FROM Route;
-Select * FROM FlightFrequency;
-
+Alter table Route
+ADD luggagePrice DECIMAL(5,3) NOT NULL DEFAULT 0
 
 Alter table Route
-ADD luggage_price DECIMAL(5,3) NOT NULL DEFAULT 0
+ADD carryOnPrice DECIMAL(5,3) NOT NULL DEFAULT 0
 
 Alter table Route
-ADD carryOn_price DECIMAL(5,3) NOT NULL DEFAULT 0
+ADD carryOnMaxWeight DECIMAL(4,2) DEFAULT 0
 
 Alter table Route
-ADD carryOn_maxWeight DECIMAL(4,2) DEFAULT 0
+Add luggageMaxWeight DECIMAL(4,2) DEFAULT 0
 
 Alter table Route
-Add luggage_maxWeight DECIMAL(4,2) DEFAULT 0
-
-Alter table Route
-Add porcentage_multiplier DECIMAL(5,2) DEFAULT 0.20
+Add porcentageMultiplier DECIMAL(5,2) DEFAULT 0.20
 
 Alter Table Flight
 Drop COLUMN priceLuggage
 
-
-SELECT COLUMN_NAME
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'Route';
-
-
-EXEC sp_rename 'Route.luggage_price', 'luggagePrice', 'COLUMN';
-EXEC sp_rename 'Route.luggage_maxWeight', 'luggageMaxWeight', 'COLUMN';
-EXEC sp_rename 'Route.carryOn_price', 'carryOnPrice', 'COLUMN';
-EXEC sp_rename 'Route.carryOn_maxWeight', 'carryOnMaxWeight', 'COLUMN';
-EXEC sp_rename 'Route.porcentage_multiplier', 'porcentageMultiplier', 'COLUMN';
-
-
 Create Table PassengerItinerary(
     idPassenger INT NOT NULL,
-	passportPassenger VARCHAR(8) NOT NULL,
     transactionId VARCHAR(20),
 
-    PRIMARY KEY (idPassenger, passportPassenger, transactionId),
+    PRIMARY KEY (idPassenger, transactionId),
 
     CONSTRAINT fk_Itinerary
     FOREIGN KEY (transactionId)
     REFERENCES Itinerary(transactionId),
 
     CONSTRAINT fk_Passenger
-    FOREIGN KEY (idPassenger, passportPassenger)
-    REFERENCES Passenger(idPassenger, passport)
+    FOREIGN KEY (idPassenger)
+    REFERENCES Passenger(idPassenger)
 );
 GO
-
 
 ALTER TABLE Itinerary 
 DROP CONSTRAINT fk_itinerary_passenger;
@@ -84,7 +63,6 @@ DROP CONSTRAINT PK__Realiza__9EAB2043FB96A356;
 
 ALTER TABLE Realiza
 DROP CONSTRAINT fk_realiza_passenger;
-
 
 ALTER TABLE Realiza
 DROP COLUMN passportPassenger;
