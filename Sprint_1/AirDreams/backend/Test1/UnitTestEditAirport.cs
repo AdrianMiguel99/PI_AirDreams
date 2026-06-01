ec062d695aea39922699510e4064b06b88db4425
JoseDavidCS <jcarrillosibaja@gmail.com>
2026-05-31 21:43:14 -0600

    Se agregaron las pruebas unitarias realizadas en las distintas funcionalidades del modulo de Aeropuertos

---
+++ Sprint_1/AirDreams/backend/Test1/UnitTestEditAirport.cs
@@ -0,0 +1,67 @@
+﻿using Moq;
+using NUnit.Framework;
+using AirDreams.API.Models.Dtos;
+using AirDreams.API.Models.Entities;
+using AirDreams.API.Repositories;
+using AirDreams.API.Services;
+
+namespace Test1
+{
+    public class UnitTestEditAirport
+    {
+        private Mock<IAirportRepository> _repositoryMock;
+        private AirportService _service;
+
+        [SetUp]
+        public void Setup()
+        {
+            _repositoryMock = new Mock<IAirportRepository>();
+            _service = new AirportService(_repositoryMock.Object);
+        }
+
+        [Test]
+        public void UpdateAsync_ExistingAirport_UpdatesSuccessfully()
+        {
+            var code = "SJO";
+            var dto = new UpdateAirportDto
+            {
+                Name = "Aeropuerto Internacional Juan Santamaría"
+            };
+
+            var airportEntity = new Airport
+            {
+                CodeAirport = code,
+                NameAirport = "Juan Santamaría", 
+                City = "San José",
+                Country = "Costa Rica"
+            };
+
+            _repositoryMock.Setup(r => r.GetByCodeAsync(code))
+                           .ReturnsAsync(airportEntity);
+            _repositoryMock.Setup(r => r.UpdateAsync(code, dto.Name))
+                           .ReturnsAsync(true);
+
+            Assert.DoesNotThrowAsync(() => _service.UpdateAsync(code, dto));
+
+            _repositoryMock.Verify(r => r.GetByCodeAsync(code), Times.Once);
+            _repositoryMock.Verify(r => r.UpdateAsync(code, dto.Name), Times.Once);
+        }
+
+        [Test]
+        public void UpdateAsync_NonExistingAirport_ThrowsKeyNotFoundException()
+        {
+            var code = "XYZ";
+            var dto = new UpdateAirportDto { Name = "Nuevo Nombre" };
+
+            _repositoryMock.Setup(r => r.GetByCodeAsync(code))
+                           .ReturnsAsync((Airport?)null);
+
+            Assert.ThrowsAsync<KeyNotFoundException>(
+                () => _service.UpdateAsync(code, dto)
+            );
+
+            _repositoryMock.Verify(r => r.GetByCodeAsync(code), Times.Once);
+            _repositoryMock.Verify(r => r.UpdateAsync(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+        }
+    }
+}
